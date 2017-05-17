42d1fc604abce623ad1ac2c9f5e5f8bfb620f6ac
Lody Borgers <hi@lodybo.nl>
2017-05-17 15:43:20 +0200

    Added menu bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { environment } from '../environments/environment';
+
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { environment } from '../environments/environment';
+
+import { MdToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -25,7 +28,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HttpModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
